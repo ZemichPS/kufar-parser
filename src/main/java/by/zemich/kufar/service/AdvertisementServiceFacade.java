ba8f34e5659f758c3b0d4db9aeeda25c8dfdacd0
Eugene <compserv@yahoo.com>
2025-01-06 19:34:01 +0300

    modify AdvertisementServiceFacade.java

--- src/main/java/by/zemich/kufar/service/AdvertisementServiceFacade.java
+++ src/main/java/by/zemich/kufar/service/AdvertisementServiceFacade.java
@@ -1,23 +1,30 @@
 package by.zemich.kufar.service;
 
+import by.zemich.kufar.dao.entity.Advertisement;
 import by.zemich.kufar.service.api.ConditionAnalyzer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class AdvertisementServiceFacade {
     private final AdvertisementService advertisementService;
     private final ConditionAnalyzer conditionAnalyzer;
 
-    public void updateAdvertisementCauseNewConditionRules(){
-        advertisementService.getAll().stream()
+    public void updateAdvertisementCauseNewConditionRules() {
+        final List<Advertisement> advertisementList = advertisementService.getAll().stream()
                 .peek(advertisement -> {
                     String details = advertisement.getDetails();
                     boolean result = conditionAnalyzer.isFullyFunctional(details);
                     advertisement.setFullyFunctional(result);
-                })
-                .forEach(advertisementService::save);
+                }).toList();
+        log.info("выполнили проверку на состояние.");
+        advertisementList.forEach(advertisementService::save);
+        log.info("выполнили сохранение всех объектов в базу");
     }
 
 }
