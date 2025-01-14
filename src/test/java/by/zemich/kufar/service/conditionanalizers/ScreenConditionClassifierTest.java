d85baa488c659d621d03f6ba2cc7fc9d6de39a4f
Eugene <compserv@yahoo.com>
2025-01-14 04:03:13 +0300

    fix ScreenConditionClassifier.java

---
+++ src/test/java/by/zemich/kufar/service/conditionanalizers/ScreenConditionClassifierTest.java
@@ -0,0 +1,39 @@
+package by.zemich.kufar.service.conditionanalizers;
+
+import by.zemich.kufar.dao.entity.Advertisement;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScreenConditionClassifierTest {
+
+    private final ScreenConditionClassifier classifier = new ScreenConditionClassifier();
+
+    @ParameterizedTest(name = "line number-> {0}, fraze-> {1}")
+    @CsvFileSource(
+            resources = "/screen_defect_details.csv",
+            delimiter = ';'
+    )
+    void analyze(int number, String description) {
+
+        Advertisement advertisement = getAdvertisement();
+        advertisement.setDetails(description);
+        boolean result = classifier.analyze(advertisement);
+        Assertions.assertTrue(result);
+    }
+
+    private static Advertisement getAdvertisement() {
+        return Advertisement.builder()
+                .subject("Some device")
+                .companyAd(false)
+                .category("17010")
+                .fullyFunctional(true)
+                .parameters(new ArrayList<>())
+                .build();
+    }
+}
